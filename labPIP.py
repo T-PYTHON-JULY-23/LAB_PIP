a36d90fc288b1d2515d24119baa99426284f4406
Khaled_Alghamdi <khaled.alghamdi0@hotmail.com>
2023-08-07 11:58:32 +0300

    LabPIP

---
+++ labPIP.py
@@ -0,0 +1,10 @@
+import art,colorama
+
+phrase1 = art.text2art('BELIEVE AND ACHEIVE',font='block')
+print(phrase1)
+phrase1=art.text2art('HELLO',font='sub-zero')
+print(phrase1)
+
+phrasebouns=art.text2art('BOUNS',font='rnd-medium')
+print(colorama.Fore.GREEN + phrasebouns)
+
