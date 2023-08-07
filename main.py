4c3a891e84dc5b0f1a40775d114ea4928cb29173
Nada Almutairi <nadaamusaad@gmail.com>
2023-08-07 12:46:48 +0300

    pip

---
+++ main.py
@@ -0,0 +1,7 @@
+from art import *
+
+print(text2art("BELIEVE ",font='block',chr_ignore=True))
+print(text2art("AND ",font='block',chr_ignore=True))
+print(text2art("ACHEIVE",font='block',chr_ignore=True))
+
+print(text2art("HELLO",font='sub-zero',chr_ignore=True))
