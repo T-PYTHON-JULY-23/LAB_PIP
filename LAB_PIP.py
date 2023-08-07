f135dfe3ec93ff93cce0280d568c7b166a9652fe
nada <nada_abdullah_8@hotmail.com>
2023-08-07 12:22:53 +0300

    lab

---
+++ LAB_PIP.py
@@ -0,0 +1,26 @@
+
+from art import *
+from colorama import *
+
+
+
+
+Art=text2art("BELIEVE AND ACHEIVE",font='block') 
+print(Art)
+
+Art1=text2art("HELLO",font='sub-zero') 
+print(Art1)
+
+
+## Bonus
+
+print(tprint("nada",font="rnd-large"))
+
+
+print(Fore.RED + 'nada ')
+print(Back.GREEN + ' nada with a green background')
+
+
+
+
+
