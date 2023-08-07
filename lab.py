75073c7eb9e56f3cd6403bc7cbdf7a6ece3d40e7
AbdulazizAlzahrani <abdulaziz.alzahrani.14@outlook.com>
2023-08-07 12:55:33 +0300

    solve lab + bonus

---
+++ lab.py
@@ -0,0 +1,18 @@
+from art import *
+from colorama import Fore
+
+tprint("BELIEVE \nAND \nACHEIVE", font='block')
+tprint("HELLO", font='sub-zero')
+
+
+txt = text2art("WELCON",font="random")
+print(Fore.RED + txt)
+
+
+txt1 = text2art("PLAY THE GAME",font="kgames_i")
+print(Fore.BLUE + txt1)
+
+
+txt2 = decor("barcode")
+print(Fore.BLUE + txt2)
+
