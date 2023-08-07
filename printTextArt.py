79dfd6676e3eae4cce2a3bc6b66f1915948d1a69
Atheer Abdullah <Atheer98.abdullah19@gmail.com>
2023-08-07 13:23:12 +0300

    lab

--- printTextArt.py
+++ printTextArt.py
@@ -1,7 +1,13 @@
 from art import *
+from colorama import *
 
 Art=text2art("BELIEVE AND ACHEIVE",font='block',chr_ignore=True) 
-print(Art)
+print(Fore.BLUE + Art)
 
 Art2=text2art("HELLO",font='sub-zero',chr_ignore=True) 
-print(Art2)
+print(Fore.GREEN + Art2)
+
+#BONUS
+Art3=text2art("BONUS",font='ascii',chr_ignore=True) 
+print(Fore.CYAN + Art3)
+
