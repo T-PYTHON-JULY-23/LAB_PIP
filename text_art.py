f525e21f9dbc2656396769703f8305eb4df23b3f
Basma <129796403+basiix1@users.noreply.github.com>
2023-08-07 11:56:09 +0300

    lab

---
+++ text_art.py
@@ -0,0 +1,7 @@
+from art import *
+
+# Print "BELIEVE AND ACHIEVE" designed with font "block"
+tprint("BELIEVE AND ACHIEVE", font="block")
+
+# Print "HELLO" designed with font "sub-zero"
+tprint("HELLO", font="sub-zero")
