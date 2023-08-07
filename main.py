2f603f8527f8f5ac0797a822c95ad0c569fec821
xluuu <mlikmozini@gmail.com>
2023-08-08 00:26:21 +0300

    Lab and bonus

---
+++ main.py
@@ -0,0 +1,14 @@
+from art import *
+from colorama import *
+tprint("BELIEVE \nAND \nACHEIVE", font = "block")
+tprint("HELLO",font = "sub-zero")
+print(Fore.RED + "HELLO")
+print(Fore.RED + text2art("HELLO","sub-zero"))
+tprint("We \nwon't leave until \nwe achieve",font="random")
+tprint("We won't leave until we achieve",font="rnd-large")
+tprint("We won't leave until we achieve",font="fancy42")
+tprint("HELLO",font="rnd-large")
+
+
+
+
