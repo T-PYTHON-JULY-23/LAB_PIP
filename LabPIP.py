5a1641fe84cc2a2b4fcb922b7d18a68608bad284
suha <suha.alrajhi01@gmail.com>
2023-08-07 13:11:27 +0300

    LabPIP suha alrajhi

---
+++ LabPIP.py
@@ -0,0 +1,15 @@
+#labPip
+from art import *
+from fontsize import *
+
+textArt1 = text2art("BELIEVE AND ACHIEVE", font='block')
+print(textArt1)
+
+textArt2 = text2art("HELLO", font='sub-zero')
+print(textArt2)
+
+
+#bonus
+tprint("LIVE YOUR LIVE", font="cybermedum")
+tprint("YOU CAN DO IT", font="rnd-xlarge")
+tprint("CODING MOOD ON", font="rnd-medium")
