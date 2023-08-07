6d56abd2c91581681525e2a7d7e871e86029f09e
Lama Alsharif <lama.a.alsharif@gmail.com>
2023-08-07 12:11:00 +0300

    Lab + bonus

---
+++ bonus.py
@@ -0,0 +1,3 @@
+from colorama import Fore, Back, Style
+
+print(Fore.GREEN + Back.WHITE +'Hi I ma lama alsharif ...')
---
+++ pip_lab.py
@@ -0,0 +1,13 @@
+from art import *
+
+text_art = tprint("BELIEVE AND ACHEIVE",font="block")
+print(text_art)
+text_art2 = tprint("HELLO",font="sub-zero")
+print(text_art2)
+phrase=text2art("Python")
+print(phrase)
+phrase2 = aprint("happy")
+print(phrase2)
+phrase3 = tprint("Python",space= 10)
+print(phrase3)
+
