ac6b63529944ef6748afc6262c8ab0dbe9c7e750
suha <suha.alrajhi01@gmail.com>
2023-08-07 13:34:42 +0300

    LabPIP + BONUS suha alrajhi

--- LabPIP.py
+++ LabPIP.py
@@ -1,6 +1,6 @@
 #labPip
 from art import *
-from fontsize import *
+from colorama import Fore
 
 textArt1 = text2art("BELIEVE AND ACHIEVE", font='block')
 print(textArt1)
@@ -10,6 +10,9 @@ print(textArt2)
 
 
 #bonus
-tprint("LIVE YOUR LIVE", font="cybermedum")
-tprint("YOU CAN DO IT", font="rnd-xlarge")
-tprint("CODING MOOD ON", font="rnd-medium")
+textArt3 =Fore.BLUE + text2art("LIVE YOUR LIVE", font="cybermedum")
+print(textArt3)
+textArt4 =Fore.GREEN + text2art("YOU CAN DO IT", font="rnd-xlarge")
+print(textArt4)
+textArt5 =Fore.YELLOW + text2art("CODING MOOD ON", font="rnd-medium")
+print(textArt5)
