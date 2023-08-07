385ec463b70a37a177cd66cb84dfbe2a1ccf3483
Basma <129796403+basiix1@users.noreply.github.com>
2023-08-07 13:22:59 +0300

    lab+bouns

--- text_art.py
+++ text_art.py
@@ -1,7 +1,14 @@
 from art import *
+import colorama
+from colorama import Fore, Style
 
-# Print "BELIEVE AND ACHIEVE" designed with font "block"
-tprint("BELIEVE AND ACHIEVE", font="block")
+# Print the phrase "BELIEVE AND ACHIEVE" with the "block" font
+block_art = text2art("BELIEVE\nAND\nACHIEVE", font="block")
+print(block_art)
 
-# Print "HELLO" designed with font "sub-zero"
-tprint("HELLO", font="sub-zero")
+my_name=text2art("Basma", font="random")
+print(my_name)
+
+# Print the phrase "HELLO" with the "sub-zero" font in red color
+sub_zero_art = text2art("HELLO", font="sub-zero")
+print(colorama.Fore.RED + sub_zero_art)
