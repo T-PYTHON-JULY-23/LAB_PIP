dfa65d0e2f428d1b21882b9419c109eb6e31e9ba
sarahnaghi <sara.naghi@gmail.com>
2023-08-07 11:58:23 +0300

    lab pip done

---
+++ LAB_PIP.py
@@ -0,0 +1,9 @@
+from art import *
+
+tprint("ART")
+
+Art=text2art("BELIEVE AND ACHEIVE",font='block',chr_ignore=True) 
+print(Art)
+
+
+tprint("Hello",font="sub-zero")
---
+++ bonus.py
@@ -0,0 +1,13 @@
+from colorama import Fore, Back, Style
+from art import *
+
+print(Fore.BLUE)
+tprint("Hi",font="zero-size")
+
+print(Fore.RED)
+tprint("Im having a good day",font="cybermedum")
+
+print(Fore.GREEN)
+tprint("Python camp",font="rnd-large")
+
+
