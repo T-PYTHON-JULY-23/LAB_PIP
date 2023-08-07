489cf7ddb160205e4a950b39ef00dc6ba355be16
ghaida <ghaida@gmail.com>
2023-08-07 13:12:51 +0300

    bouns done

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 
 from art import *
+from colorama import*
 
 
 
-print(text2art("BELIEVE",font='block',chr_ignore=True))
-print(text2art("AND",font='block',chr_ignore=True))
-print(text2art("ACHEIVE",font='block',chr_ignore=True))
-print(text2art("HELLO",font='sub-zero',))
+print(Fore.BLUE +(text2art("BELIEVE",font='block',chr_ignore=True)))
+print(Style.RESET_ALL)
+print(Fore.RED +(text2art("AND",font='block',chr_ignore=True)))
+
+print(Fore.GREEN +(text2art("ACHEIVE",font='block',chr_ignore=True)))
+print(Fore.LIGHTMAGENTA_EX +(text2art("HELLO",font='sub-zero',)))
+print(Style.RESET_ALL)
+tprint("GHAIDA",font='random')
+
+print(Style.RESET_ALL)
