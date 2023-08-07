fb514a03e3e09eca13d6078e165b19f6b4afb46b
naif <naif.albariqi201@hotmail.com>
2023-08-07 12:38:05 +0300

    lab-bo --N

---
+++ main.py
@@ -0,0 +1,9 @@
+
+from art import *
+from colorama import Fore, Back, Style
+
+
+Art = text2art("BELIEVE \nAND\n ACHEIVE", font='block')
+print(Art+Fore.RED)
+art1=text2art("HELLO", font='sub-zero')
+print(art1+Fore.RED)
