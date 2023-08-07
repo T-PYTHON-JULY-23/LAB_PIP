707487b33f51da23ca27711a1ac2beb0d1f2b32d
sara <sara@hotmail.com>
2023-08-07 12:47:27 +0300

    Lab + Bonus

---
+++ main.py
@@ -0,0 +1,15 @@
+from art import *
+
+from colorama import Fore, Back, Style
+text= text2art("BELIEVE AND ACHEIVE",font="block", chr_ignore=True)
+print(text)
+
+text2= text2art("HELLO",font="sub-zero")
+print(text2)
+
+text3= text2art("SARAH",font="random")
+print(text3)
+
+
+print(Fore.RED + 'Bonus')
+
