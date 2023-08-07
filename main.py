6e3adf9352aaec5b33020b318c93c10c414438fd
renad1 <renadxx17@gmail.com>
2023-08-07 11:53:24 +0300

    lab

---
+++ main.py
@@ -0,0 +1,9 @@
+from art import *
+Art=text2art("art")
+print(Art)
+tprint("BELIEVE",font="block",chr_ignore=True)
+tprint("AND",font="block",chr_ignore=True)
+tprint("ACHEIVE",font="block",chr_ignore=True)
+tprint("HELLO",font="sub-zero",chr_ignore=True)
+tprint("Python",font="wavy",chr_ignore=True)
+tprint("Python",font="twisted",chr_ignore=True)
