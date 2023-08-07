e88804e2c984ce2de494450b90b1def0659679f2
Nada Almutairi <nadaamusaad@gmail.com>
2023-08-07 13:13:04 +0300

    done

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from art import *
+from colorama import *
+
 
 print(text2art("BELIEVE ",font='block',chr_ignore=True))
 print(text2art("AND ",font='block',chr_ignore=True))
 print(text2art("ACHEIVE",font='block',chr_ignore=True))
 
-print(text2art("HELLO",font='sub-zero',chr_ignore=True))
+print(Fore.BLUE+(text2art("HELLO",font='sub-zero',chr_ignore=True)))
+
+tprint("Nada","rnd-xlarge")
